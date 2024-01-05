777d4a232fb6d282144ba9c70b12c8d229c52fa4
Hhertilien1 <kinghertilien@gmail.com>
2024-01-05 10:45:16 -0500

    DONE

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class Wordsmith
+	{
+		public bool IsAPalindrome(string word)
+		{
+            var wordCharrArr = word.ToCharArray();
+            Array.Reverse(wordCharrArr);
+            var reversed = new string(wordCharrArr);
+
+            if (word.ToLower() == reversed.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+	}
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
-    public class UnitTest1
+   public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("civic",true)]
+        [InlineData("hey", false)]
+        [InlineData("RaCeCaR", true)]
+        [InlineData("hello", false)]
+        [InlineData("mom", true)]
+        [InlineData("dad", true)]
+        [InlineData("herb", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
-
+            var wordsmith = new Wordsmith();
+            var actual = wordsmith.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
+           
         }
     }
 }
